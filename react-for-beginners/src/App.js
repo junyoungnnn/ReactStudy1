ede4f1489ea6681b04b10efa126734fe3f345b2a
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-03-18 17:39:16 +0900

    2024.03.18

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,13 +1,31 @@
-import Button from "./Button"
-import styles from "./App.module.css"
+import {useState, useEffect} from "react";
+
+function Hello(){
+    // function HiFn(){
+    //     console.log("created :)");
+    //     return ByFn;
+    // }
+    // function ByFn(){
+    //     console.log("bye :(");
+    // }
+    useEffect(() =>{
+        console.log("hi :)");
+        return ()=>{
+            console.log("bye :(");
+        }
+    }, []);
+    return <h1>Hello</h1>;
+}
 
 function App() {
-  return (
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
+    return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"}/>
+        {showing ? <Hello/> : null}
+        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
-  );
+    );
 }
 
 export default App;
