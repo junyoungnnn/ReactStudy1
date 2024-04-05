a6d863d3e62921b143606b5a411832de15ecb18f
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-04-05 18:43:37 +0900

    2024.04.05

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,39 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if(toDo === "" )
-        {
-            return;
-        }
-        setToDos(currentArray => [toDo, ...currentArray]);
-        setToDo("");
-    };
-    console.log(toDos);
-    return (
+function App()
+{
+const [loading, setLoading] = useState(true);
+const [movies, setMovies] = useState([]);
+const getMovies = async () => {
+  const json = await (
+    await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+  ).json();
+  setMovies(json.data.movies);
+  setLoading(false);
+};
+useEffect(() =>{
+  // fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
+  // .then(response => response.json())
+  // .then((json) => setMovies(json.data.movies));
+  // setLoading(false);
+  getMovies();
+}, []);
+console.log(movies);
+return (
+  <div>
+    {loading ? (
+      <h1>loading...</h1>
+    ) : (
       <div>
-        <h1>My To Dos ({toDos.length})</h1>
-        <form onSubmit={onSubmit}>
-          <input
-            onChange={onChange}
-            value={toDo}
-            type="text"
-            placeholder="Write your to do..."
-          />
-          <button>Add To Do</button>
-        </form>
-        <hr />
-        <ul>
-          {toDos.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image}></img>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
-    );
+    )}
+  </div>
+);
 }
 
 export default App;
