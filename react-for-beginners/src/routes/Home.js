e4bb4672f94b49983685809b75bd1dcc8822603b
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-05-14 01:23:43 +0900

    parameters

--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -2,24 +2,18 @@ import { useEffect, useState } from "react";
 import Movie from "../components/Movie.js";
 function Home(){
     const [loading, setLoading] = useState(true);
-const [movies, setMovies] = useState([]);
-const getMovies = async () => {
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
   const json = await (
-    await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
+    await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
   ).json();
   setMovies(json.data.movies);
   setLoading(false);
 };
 useEffect(() =>{
-  // fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
-  // .then(response => response.json())
-  // .then((json) => setMovies(json.data.movies));
-  // setLoading(false);
   getMovies();
 }, []);
-console.log(movies);
+//console.log(movies);
 return (
   <div>
     {loading ? (
@@ -29,6 +23,7 @@ return (
         {movies.map((movie) => (
         <Movie
             key={movie.id}
+            id={movie.id}
             coverImg={movie.medium_cover_image} 
             title={movie.title} 
             summary={movie.summary} 
